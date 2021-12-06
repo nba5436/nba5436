f9228710d4fb2efc18358ddbd10a0632d52ee0d0
nba5436 <89338901+nba5436@users.noreply.github.com>
2021-12-07 00:23:46 +0900

    Update upbit_api.py

--- upbit_api.py
+++ upbit_api.py
@@ -27,18 +27,20 @@ upbit = pyupbit.Upbit(access, secret)
 print("autoTrade start")
 btc_mp = 0
 eth_mp = 0
+xrp_mp = 0
 
 # 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-BTC") - datetime.timedelta(minutes=30)
         end_time = start_time + datetime.timedelta(minutes=1)
+        
+        df = pyupbit.get_ohlcv("KRW-ETH", count=1)
+        
         print(now)
         print(start_time)
         print(end_time)
-        #print(btc_mp)
-        #print(eth_mp)
 
         if now > end_time:
             btc_mp = 0
@@ -47,19 +49,24 @@ while True:
         if start_time <= now < end_time:
             krw = get_balance("KRW")
             print(krw)
-            btc = get_balance("BTC")
-            print(btc)
-            eth = get_balance("ETH")
-            print(eth)
-            if krw > 50000:
-                if btc_mp == 0:
-                    upbit.buy_market_order("KRW-BTC", 50000)
-                    btc_mp = 1
-                    print(btc_mp)
-                if eth_mp == 0:
-                    upbit.buy_market_order("KRW-ETH", 50000)
-                    eth_mp = 1
-                    print(eth_mp)
+            if krw > 5000:
+                if xrp_mp == 0:
+                    if float(df['open']) > float(df['close']):
+                        upbit.buy_market_order("KRW-XRP", 365297)
+                        xrp_mp = 1
+                        print(xrp_mp)
+                    else:
+                        upbit.buy_market_order("KRW-XRP", 182648)
+                        xrp_mp = 1
+                        print(xrp_mp)
+                #if btc_mp == 0:
+                #    upbit.buy_market_order("KRW-BTC", 50000)
+                #    btc_mp = 1
+                #    print(btc_mp)
+                #if eth_mp == 0:
+                #    upbit.buy_market_order("KRW-ETH", 50000)
+                #    eth_mp = 1
+                #    print(eth_mp)
         
     except Exception as e:
         print(e)
